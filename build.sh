94e35b3bfdd24838fff5d97115fcdb1c06e7026b
Christian Häussler <c-n-i@web.de>
2022-07-26 21:01:16 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+version="0.8.5"
+image="srbminer-multi"
+docker build . --tag docker.io/cniweb/$image:$version
+docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:$version
+docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:latest
+docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
+docker push ghcr.io/cniweb/$image:$version
+docker push ghcr.io/cniweb/$image:latest
+docker push docker.io/cniweb/$image:$version
+docker push docker.io/cniweb/$image:latest
