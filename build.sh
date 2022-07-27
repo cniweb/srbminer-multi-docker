dce22315a0fbe1881afd5da43f9f26f09dd9d3db
Christian Häussler <c-n-i@web.de>
2022-07-27 17:11:40 +0200

    Add quay.io docker Registry

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 version="1.0.2"
 image="srbminer-multi"
 docker build . --tag docker.io/cniweb/$image:$version
+docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
 docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:latest
-docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
-docker push ghcr.io/cniweb/$image:$version
-docker push ghcr.io/cniweb/$image:latest
+docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:$version
+docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:latest
 docker push docker.io/cniweb/$image:$version
 docker push docker.io/cniweb/$image:latest
+docker push ghcr.io/cniweb/$image:$version
+docker push ghcr.io/cniweb/$image:latest
+docker push quay.io/cniweb/$image:$version
+docker push quay.io/cniweb/$image:latest
