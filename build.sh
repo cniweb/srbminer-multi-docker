dfa8592def31f6e70e545f7602a3e0d9e8f742ae
Christian Häussler <c-n-i@web.de>
2022-11-21 00:13:42 +0100

    Bump to version 1.1.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="1.1.0"
+version="1.1.1"
 image="srbminer-multi"
 docker build . --tag docker.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
