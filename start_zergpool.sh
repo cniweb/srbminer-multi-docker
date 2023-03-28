87f30a95f9bebc405a7395a750815d43b1be2a08
Christian Häussler <c-n-i@web.de>
2023-03-28 23:04:39 +0200

    Add HOSTNAME as Password

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD $EXTRAS"
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD $EXTRAS
+echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD-$HOSTNAME $EXTRAS"
+./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD-$HOSTNAME $EXTRAS
