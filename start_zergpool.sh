7819ec02ffe0e5038daef5bba05584f4536958a5
Christian Häussler <c-n-i@web.de>
2023-05-16 08:38:53 +0200

    Add cpu_threads

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD-$HOSTNAME $EXTRAS"
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD-$HOSTNAME $EXTRAS
+cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
+echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD-$HOSTNAME $EXTRAS --cpu-threads $cpu_threads"
+./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD-$HOSTNAME $EXTRAS --cpu-threads $cpu_threads
