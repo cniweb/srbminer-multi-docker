0f9fbd59c69e05d0adaceca66de1ff9f0dc8ef98
Christian Häussler <c-n-i@web.de>
2023-06-12 23:33:01 +0200

    Bump to version 2.2.8
    Signed-off-by: Christian Häussler <c-n-i@web.de>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="2.2.6"
+version="2.2.8"
 image="srbminer-multi"
 docker build . --tag docker.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
