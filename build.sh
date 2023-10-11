4c20622385e7ac8d2b5b1457e8a32abb125b8e37
Christian Häussler <c-n-i@web.de>
2023-10-11 23:26:13 +0200

    Bump to version 2.3.7

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="2.3.5"
+version="2.3.7"
 image="srbminer-multi"
 docker build . --tag docker.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
