616688a6627895fc52145c9ba89e26274b7c3b4d
Christian Häussler <c-n-i@web.de>
2024-02-14 22:44:57 +0100

    Fix password parameter

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper $EXTRAS --cpu-threads $cpu_threads"
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper $EXTRAS --cpu-threads $cpu_threads
+echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads"
+./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads
